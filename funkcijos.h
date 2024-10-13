96688a0d426485d3eebc55f05f6b8bdea6c6a3c9
karina-stur <sturonaite.k@gmail.com>
2024-10-13 14:35:29 +0300

    Update funkcijos.h

--- funkcijos.h
+++ funkcijos.h
@@ -5,11 +5,11 @@
 #include <string>
 #include "studentas.h"
 
-void nuskaititiIsFailo(const std::string& failoPavadinimas, std::vector<Studentas>& studentai);
-double skaiciuotiGalutiniVidurki(const std::vector<int>& namuDarbai, int egzaminas);
-double skaiciuotiGalutiniMediana(std::vector<int> namuDarbai, int egzaminas);
-void surikiuotiStudentus(std::vector<Studentas>& studentai);
 void generuotiStudentuFailus();
+void nuskaitytiStudentus(const std::string& failoPavadinimas, std::vector<Studentas>& studentai);
+double skaiciuotiGalutiniVidurki(const std::vector<int>& namuDarbai, int egzaminas);
+double skaiciuotiGalutiniMediana(const std::vector<int>& namuDarbai, int egzaminas);
+double isaugotiStudentuGrupe(const std::vector<Studentas>& studentai, const std::string& failoPavadinimas, bool pagalVidurki);
+void grupuotiIrIssaugotiStudentus(std::vector<Studentas>& studentai, bool pagalVidurki);
 
 #endif // FUNKCIJOS_H
-
