59a61f52ef81335fdd7632c25e09b368a9bc138f
karina-stur <sturonaite.k@gmail.com>
2024-10-24 14:18:30 +0300

    Update funkcijos.h

--- funkcijos.h
+++ funkcijos.h
@@ -2,13 +2,25 @@
 #define FUNKCIJOS_H
 
 #include <vector>
+#include <list>
 #include <string>
 #include "studentas.h"
 
-void nuskaititiIsFailo(const std::string& failoPavadinimas, std::vector<Studentas>& studentai);
+void generuotiStudentuFailus();
+
+template <typename Container>
+void nuskaitytiStudentus(const std::string& failoPavadinimas, Container& studentai);
+
 double skaiciuotiGalutiniVidurki(const std::vector<int>& namuDarbai, int egzaminas);
-double skaiciuotiGalutiniMediana(std::vector<int> namuDarbai, int egzaminas);
-void surikiuotiStudentus(std::vector<Studentas>& studentai);
+double skaiciuotiGalutiniMediana(const std::vector<int>& namuDarbai, int egzaminas);
+
+template <typename Container>
+double isaugotiStudentuGrupe(const Container& studentai, const std::string& failoPavadinimas, bool pagalVidurki);
+
+template <typename Container>
+void grupuotiIrIssaugotiStudentus(Container& studentai, bool pagalVidurki);
+
+void testKonteinerius(const std::string& failoPavadinimas, bool pagalVidurki);
+
 
 #endif // FUNKCIJOS_H
-
